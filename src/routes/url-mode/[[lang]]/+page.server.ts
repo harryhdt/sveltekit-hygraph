2f27f00a6bc250e262dc593385cc75a42be961d2
Harry H <harryhdtofficial@gmail.com>
2024-12-13 16:26:50 +0800

    try cache strategy

--- src/routes/url-mode/[[lang]]/+page.server.ts
+++ src/routes/url-mode/[[lang]]/+page.server.ts
@@ -1,14 +1,23 @@
 import { getProducts, language } from '$lib/server/api';
-import { cacheHeaders } from '$lib/server/api/cache';
+import { AppCache, cacheHeaders } from '$lib/server/api/cache';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import type { ProductType } from '$lib/type';
 
 export const load: PageServerLoad = async ({ params, setHeaders }) => {
 	const lang = language(params.lang);
 	//
-	const data = await getProducts(lang);
+	const cacheKey = `${lang}-products`;
+	let data = await AppCache.get<ProductType[] | null>(cacheKey);
 	if (!data) {
-		error(500, 'Something went wrong');
+		data = await getProducts(lang);
+		if (!data) {
+			// handle error
+			error(500, 'Something went wrong');
+		}
+		await AppCache.set(cacheKey, data);
+	} else {
+		console.log(`Cache hit: ${cacheKey}`);
 	}
 	//
 	setHeaders(cacheHeaders(60));
--- src/routes/url-mode/[[lang]]/p/[slug]/+page.server.ts
+++ src/routes/url-mode/[[lang]]/p/[slug]/+page.server.ts
@@ -1,14 +1,23 @@
 import { getProduct, language } from '$lib/server/api';
-import { cacheHeaders } from '$lib/server/api/cache';
+import { AppCache, cacheHeaders } from '$lib/server/api/cache';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import type { ProductType } from '$lib/type';
 
 export const load: PageServerLoad = async ({ params, setHeaders }) => {
 	const lang = language(params.lang);
 	//
-	const data = await getProduct(lang, params.slug);
+	const cacheKey = `${lang}-product-${params.slug}`;
+	let data = await AppCache.get<ProductType | null>(cacheKey);
 	if (!data) {
-		error(404, 'Product not found');
+		data = await getProduct(lang, params.slug);
+		if (!data) {
+			// handle error
+			error(404, 'Product not found');
+		}
+		await AppCache.set(cacheKey, data);
+	} else {
+		console.log(`Cache hit: ${cacheKey}`);
 	}
 	//
 	setHeaders(cacheHeaders(60));
